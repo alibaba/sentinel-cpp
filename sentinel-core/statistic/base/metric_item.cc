c508171f98d55027316375d4b7261738b6c7b6d1
Eric Zhao <sczyh16@gmail.com>
2019-05-07 11:11:59 +0800

    Update MetricItem and add test cases
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/statistic/base/metric_item.cc
+++ sentinel-core/statistic/base/metric_item.cc
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <sstream>
 #include <string>
+#include <vector>
+
+#include "absl/strings/str_split.h"
 
 #include "sentinel-core/statistic/base/metric_item.h"
 
@@ -20,7 +23,8 @@ std::string MetricItem::ToThinString() const {
 std::string MetricItem::ToFatString() const {
   std::stringstream ss;
   ss << timestamp_ << '|';
-  // TODO: data format needed (yyyy-MM-dd HH:mm:ss).
+  // TODO: date format needed (yyyy-MM-dd HH:mm:ss).
+  ss << timestamp_ << '|';
   std::string legal_name = resource_;
   std::replace(legal_name.begin(), legal_name.end(), '|', '_');
   ss << legal_name << '|' << pass_qps_ << '|' << block_qps_ << '|'
@@ -29,11 +33,43 @@ std::string MetricItem::ToFatString() const {
 }
 
 MetricItemPtr MetricItem::FromThinString(const std::string& thin_str) {
-  return nullptr;  // TODO
+  try {
+    std::vector<std::string> vec = absl::StrSplit(thin_str, '|');
+    if (vec.size() < 7) {
+      return nullptr;
+    }
+    MetricItemPtr item = std::make_shared<MetricItem>();
+    item->set_timestamp(std::stoll(vec[0]));
+    item->set_resource(vec[1]);
+    item->set_pass_qps(std::stoll(vec[2]));
+    item->set_block_qps(std::stoll(vec[3]));
+    item->set_complete_qps(std::stoll(vec[4]));
+    item->set_exception_qps(std::stoll(vec[5]));
+    item->set_rt(std::stoll(vec[6]));
+    return item;
+  } catch (std::exception& ex) {
+    return nullptr;
+  }
 }
 
 MetricItemPtr MetricItem::FromFatString(const std::string& fat_str) {
-  return nullptr;  // TODO
+  try {
+    std::vector<std::string> vec = absl::StrSplit(fat_str, '|');
+    if (vec.size() < 8) {
+      return nullptr;
+    }
+    MetricItemPtr item = std::make_shared<MetricItem>();
+    item->set_timestamp(std::stoll(vec[0]));
+    item->set_resource(vec[2]);
+    item->set_pass_qps(std::stoll(vec[3]));
+    item->set_block_qps(std::stoll(vec[4]));
+    item->set_complete_qps(std::stoll(vec[5]));
+    item->set_exception_qps(std::stoll(vec[6]));
+    item->set_rt(std::stoll(vec[7]));
+    return item;
+  } catch (std::exception& ex) {
+    return nullptr;
+  }
 }
 
 }  // namespace Stat
