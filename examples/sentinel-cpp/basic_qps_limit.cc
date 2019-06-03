be1e6813fcffec22f15c0721c6a13c0488f7ec9e
Eric Zhao <sczyh16@gmail.com>
2019-06-03 20:01:58 +0800

    Add logs and update demo
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- examples/sentinel-cpp/basic_qps_limit.cc
+++ examples/sentinel-cpp/basic_qps_limit.cc
@@ -12,14 +12,11 @@
 #include "sentinel-core/public/sph_u.h"
 #include "sentinel-core/transport/command/http_server_init_target.h"
 
-Sentinel::Init::InitTargetRegister<
-    Sentinel::Transport::HttpCommandCenterInitTarget>
-    HCCIT_registered;
-
-void doEntry(const char* resource) {
+void DoEntry(const char* resource) {
   while (true) {
     auto r = Sentinel::SphU::Entry(resource);
     if (r->IsBlocked()) {
+      // Indicating the request is blocked. We can do something for this.
       std::cout << "b";
       std::this_thread::sleep_for(std::chrono::milliseconds(10));
     } else {
@@ -30,29 +27,32 @@ void doEntry(const char* resource) {
   }
 }
 
-void doOneEntry() { doEntry("my_open_api_abc"); }
+void DoOneEntry() { DoEntry("my_open_api_abc"); }
 
-void doAnotherEntry() { doEntry("m1:my_another_api_233"); }
+void DoAnotherEntry() { DoEntry("m1:my_another_api_233"); }
 
 /*
  * Run the demo: CSP_SENTINEL_APP_NAME=your-app-name
  * ./bazel-bin/examples/sentinel-cpp/sentinel_basic_qps_limit
  */
 int main() {
-  Sentinel::Log::MetricLogTask task;
-  task.Initialize();
+  // Initialize for Sentinel.
+  Sentinel::Transport::HttpCommandCenterInitTarget command_center_init;
+  command_center_init.Initialize();
+  Sentinel::Log::MetricLogTask metric_log_task;
+  metric_log_task.Initialize();
 
   Sentinel::Flow::FlowRule rule1{"my_open_api_abc"};
   rule1.set_count(10);
   Sentinel::Flow::FlowRule rule2{"m1:my_another_api_233"};
   rule2.set_count(5);
   Sentinel::Flow::FlowRuleManager::GetInstance().LoadRules({rule1, rule2});
-  std::thread t1(doOneEntry);
-  std::thread t2(doOneEntry);
+  std::thread t1(DoOneEntry);
+  std::thread t2(DoOneEntry);
   std::this_thread::sleep_for(std::chrono::milliseconds(5));
-  std::thread t3(doOneEntry);
-  std::thread t4(doAnotherEntry);
-  std::thread t5(doOneEntry);
+  std::thread t3(DoOneEntry);
+  std::thread t4(DoAnotherEntry);
+  std::thread t5(DoOneEntry);
   t1.join();
   t2.join();
   t3.join();
