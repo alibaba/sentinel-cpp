e5204c7bf2b2c156a0357aa7076ab557eb418eb1
Eric Zhao <sczyh16@gmail.com>
2019-06-10 11:26:47 +0800

    Define the constant string with explicit type to avoid wrong type inference

--- sentinel-core/common/constants.h
+++ sentinel-core/common/constants.h
@@ -6,10 +6,10 @@ namespace Constants {
 static constexpr int kDefaultSampleCount = 2;
 static constexpr int kDefaultIntervalMs = 1000;
 
-static constexpr auto kLimitOriginDefault{"default"};
-static constexpr auto kLimitOriginOther{"other"};
+static constexpr const char* kLimitOriginDefault = "default";
+static constexpr const char* kLimitOriginOther = "other";
 
-static constexpr auto kDefaultContextName{"sentinel_default_context"};
+static constexpr const char* kDefaultContextName = "sentinel_default_context";
 
 static constexpr int kMaxAllowedRt = 4900;
 
