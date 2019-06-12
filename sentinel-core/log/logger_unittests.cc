b2f8a92d10584948f63a57972bb2f9d21ce24ac0
Eric Zhao <sczyh16@gmail.com>
2019-06-12 11:58:23 +0800

    Fix test failure for rolling date log
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/log/logger_unittests.cc
+++ sentinel-core/log/logger_unittests.cc
@@ -11,13 +11,23 @@
 #include <iostream>
 
 #include "sentinel-core/log/logger.h"
+#include "sentinel-core/utils/time_utils.h"
+
+#include "absl/strings/str_format.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
+
+using namespace Sentinel::Utils;
 
 namespace Sentinel {
 namespace Log {
 
 namespace {
-size_t GetLineNumberFromFilePath(const std::string& filename) {
-  std::ifstream inFile(filename);
+size_t GetLineNumberFromFilePathWithDate(const std::string& filename) {
+  auto real_filename = absl::StrFormat(
+      "%s_%s", filename,
+      absl::FormatTime("%Y-%m-%d", absl::Now(), absl::LocalTimeZone()));
+  std::ifstream inFile(real_filename);
   return std::count(std::istreambuf_iterator<char>(inFile),
                     std::istreambuf_iterator<char>(), '\n');
 }
@@ -31,31 +41,35 @@ TEST(LoggerTest, BasicConstruct) {
 
 TEST(LoggerTest, BasicLogger) {
   Logger::Uninitialization();
-  std::string log_file_name = std::tmpnam(nullptr);
+  std::string log_file_name =
+      absl::StrFormat("./test-%d", TimeUtils::CurrentTimeMillis().count());
+
   ASSERT_TRUE(!log_file_name.empty());
   ASSERT_TRUE(Logger::InitDefaultLogger(log_file_name, ""));
   SENTINEL_LOG(info, "test one");
   SENTINEL_LOG(info, "test two");
   Logger::FlushAllLogger();
   sync();
-  EXPECT_EQ(2, GetLineNumberFromFilePath(log_file_name));
+  EXPECT_EQ(2, GetLineNumberFromFilePathWithDate(log_file_name));
   SENTINEL_LOG(warn, "test one");
   SENTINEL_LOG(warn, "test two");
   Logger::FlushAllLogger();
   sync();
-  EXPECT_EQ(4, GetLineNumberFromFilePath(log_file_name));
+  EXPECT_EQ(4, GetLineNumberFromFilePathWithDate(log_file_name));
   // Default log level is warn
   SENTINEL_LOG(debug, "test one");
   SENTINEL_LOG(debug, "test two");
   Logger::FlushAllLogger();
   sync();
-  EXPECT_EQ(4, GetLineNumberFromFilePath(log_file_name));
+  EXPECT_EQ(4, GetLineNumberFromFilePathWithDate(log_file_name));
   Logger::SetAllLoggerLevel(trace);
   SENTINEL_LOG(trace, "test one");
   SENTINEL_LOG(trace, "test two");
   Logger::FlushAllLogger();
   sync();
-  EXPECT_EQ(6, GetLineNumberFromFilePath(log_file_name));
+  EXPECT_EQ(6, GetLineNumberFromFilePathWithDate(log_file_name));
+
+  remove(log_file_name.c_str());
 }
 
 }  // namespace Log
