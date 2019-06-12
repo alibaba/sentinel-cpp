8733e3e43769be80b710bcd61a6e7d6d320a20fd
Eric Zhao <sczyh16@gmail.com>
2019-06-12 15:55:43 +0800

    Add back LogSlot to default slot chain (#15)
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/slot/global_slot_chain.h
+++ sentinel-core/slot/global_slot_chain.h
@@ -2,11 +2,7 @@
 
 #include <memory>
 
-#include "sentinel-core/flow/flow_slot.h"
-#include "sentinel-core/slot/base/default_slot_chain_impl.h"
-#include "sentinel-core/slot/log_slot.h"
-#include "sentinel-core/slot/resource_node_builder_slot.h"
-#include "sentinel-core/slot/statistic_slot.h"
+#include "sentinel-core/slot/base/slot_chain.h"
 
 namespace Sentinel {
 namespace Slot {
