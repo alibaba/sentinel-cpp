14941409b13f315bff7f581523c4d401d0d64736
Eric Zhao <sczyh16@gmail.com>
2019-06-19 18:09:57 +0800

    Add version command handler
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-core/transport/command/handler/version_handler.cc
@@ -0,0 +1,14 @@
+#include "sentinel-core/transport/command/handler/version_handler.h"
+
+#include <string>
+
+namespace Sentinel {
+namespace Transport {
+
+CommandResponsePtr VersionCommandHandler::Handle(
+    const CommandRequest& request) {
+  return CommandResponse::OfSuccess("0.1.0");
+}
+
+}  // namespace Transport
+}  // namespace Sentinel
